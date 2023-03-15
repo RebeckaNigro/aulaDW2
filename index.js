4a9ca89a1fe1facdaa5585eb5370b727c2114711
aluno <rebeckanigro03@gmail.com>
2023-03-15 17:12:40 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    var itens = document.getElementsByTagName('li');
+    alert("Itens: " + itens.length);
+
+    for (let index = 0; index < itens.length; index++) {
+        const element = itens[index];
+        
+        element.innerText = "Professor " + element.innerText;
+    }
+}
+
